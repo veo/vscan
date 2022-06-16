c9c30f30acd4fb0afb5ce5972e567dd8f72e493a
veo <38176360+veo@users.noreply.github.com>
2022-06-16 10:13:30 +0800

    vscan

--- main.go
+++ main.go
@@ -11,16 +11,16 @@ func main() {
 	if runtime.GOOS == "windows" {
 		options.NoColor = true
 	}
-	naabuRunner, err := naabuRunner.NewRunner(options)
+	runner, err := naabuRunner.NewRunner(options)
 	if err != nil {
 		gologger.Fatal().Msgf("Could not create runner: %s\n", err)
 	}
-	err = naabuRunner.RunEnumeration()
+	err = runner.RunEnumeration()
 	if err != nil {
 		gologger.Fatal().Msgf("Could not run enumeration: %s\n", err)
 	}
 	gologger.Info().Msg("Port scan over,web scan starting")
-	err = naabuRunner.Httpxrun()
+	err = runner.Httpxrun()
 	if err != nil {
 		gologger.Fatal().Msgf("Could not run httpRunner: %s\n", err)
 	}
